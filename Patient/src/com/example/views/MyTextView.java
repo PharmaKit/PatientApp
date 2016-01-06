c8cb316d05b976953a676d8419b6ddd9d88d4278
VarunBarve <varun7691@gmail.com>
2016-01-06 11:43:19 +0530

    Adding View package for fonts

---
+++ Patient/src/com/example/views/MyTextView.java
@@ -0,0 +1,21 @@
+package com.example.views;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class MyTextView extends TextView {
+
+	public MyTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		if (isInEditMode()) {
+			return;
+		}
+
+		Typeface typeface = Typeface.createFromAsset(context.getAssets(),
+				"SofiaProLight.otf");
+		setTypeface(typeface);
+	}
+}
